98dd7db5aaa35f5aa3f25857a5b20c2401c65b7b
陈发浩 <chenfahao@wlan-napt-003.u-aizu.ac.jp>
2022-11-16 10:43:43 +0900

    add pages

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+
+const navMenuItems = document.querySelectorAll(
+    ".nav-menu li"
+  );
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("open")
+
+    navMenuItems.forEach((item, index) => {
+        // 如果已添加animation,先取消
+        if (item.style.animation) {
+          item.style.animation = "";
+        } else {
+          item.style.animation = `0.3s ease-in slideIn forwards ${index * 0.1 + 0.3}s`;
+        }
+      });
+});
